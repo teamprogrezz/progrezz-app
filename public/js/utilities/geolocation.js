ad4dfb22bf44a1e50f24cbf3d6ce45af23b98b1e
Cristo González Rodríguez <cristogr.93@gmail.com>
2015-04-13 20:59:36 +0100

    Modificación parámetros de geolocalización

--- public/js/utilities/geolocation.js
+++ public/js/utilities/geolocation.js
@@ -23,16 +23,16 @@ var GeolocationUtils = {};
   namespace.Config = {
     
     ENABLE_HIGH_ACCURACY: false,
-    TIMEOUT: 5000, // ms
-    MAXIMUM_AGE: 2000 // ms
+    TIMEOUT: 15000, // ms
+    MAXIMUM_AGE: 30000 // ms
   }
   
   namespace.distanceTwoGeoPoints = function(lat1, lon1, lat2, lon2) {
     var R = 6371; // Radio medio de la tierra (km)
     var dLat = (lat2 - lat1).toRad();
     var dLon = (lon2 - lon1).toRad();
-    var lat1 = lat1.toRad();
-    var lat2 = lat2.toRad();
+    lat1 = lat1.toRad();
+    lat2 = lat2.toRad();
     var a = Math.pow(Math.sin(dLat/2), 2) + Math.pow(Math.sin(dLon/2), 2) * Math.cos(lat1) * Math.cos(lat2); 
     var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
     var d = R * c * 1000; // Distancia (m)
